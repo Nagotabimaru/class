8bdd67311f691de989bdd506562814c47e822594
Nagotabimaru <kruz.r@list.ru>
2023-02-09 09:34:23 +0400

    Initial commit

---
+++ class.py
@@ -0,0 +1,15 @@
+class Groundtransport(object): #Наземный транспорт name гусеничный транспорт
+    def __init__(self, name, caterpillartransport):
+        self.name = name
+        self.caterpillartransport = caterpillartransport
+
+class AirCushionTransport(object): #транспорт на воздушной подушке
+    def __init__(self, air):
+        self.air = air
+
+Aircushion = AirCushionTransport('Машина на воздушной подушке - SR-N1')
+car = Groundtransport('Машина на  гусеницах - Renault 12\n', Aircushion)
+
+print("{}{}".format(car.name, car.caterpillartransport.air))
+
+# Как-то так ? или я усложнил 
